2e089bf59661550b2e6282c6200fa65ed27dab84
Karkin Alexandr <karkinalex96@gmail.com>
2017-10-29 20:53:31 +0300

    front-end: add photo upload for signup pages

--- app/views.lua
+++ app/views.lua
@@ -13,18 +13,21 @@ box.cfg {
 function views.homepage(self)
     local cookie = self:cookie('tarantool_id')
     local is_auth = box.space.sessions:select({cookie})
+    local resp;
     if (table.getn(is_auth) ~= 0) then
         if (is_auth[1][3] == 1) then
             local designer = box.space.designer.index.email:select({is_auth[1][2]})
-            return self:render({isAuthorized=1, name = designer[1][5], status=1, photo = designer[1][9]})
+            resp = self:render({isAuthorized=1, name = designer[1][5], status=1, photo = designer[1][9]})
         else
             local showroom = box.space.showroom.index.email:select({is_auth[1][2]})
             log.info(showroom[1][8])
-            return self:render({isAuthorized=1, name = showroom[1][2], status=2, photo = showroom[1][8][1]})
+            resp = self:render({isAuthorized=1, name = showroom[1][2], status=2, photo = showroom[1][8][1]})
         end
     else
-        return self:render({isAuthorized=0})
+        resp = self:render({isAuthorized=0})
     end
+    resp.headers['Access-Control-Allow-Origin'] = 'file:///*'
+    return resp
 end
 
 
